bf52ef5f3c8872324bc332eb99cf3f79d2c92d21
James Welch <james.welch@cs.ox.ac.uk>
2022-05-04 20:55:16 +0100

    Temporary fix for pdf generation, and add a test to show pdf generation works

---
+++ src/test/groovy/uk/ac/ox/softeng/dita/test/DitaProcessorSpecification.groovy
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020-2022 University of Oxford and Health and Social Care Information Centre, also known as NHS Digital
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package uk.ac.ox.softeng.dita.test
+
+import uk.ac.ox.softeng.maurodatamapper.dita.DitaProject
+import uk.ac.ox.softeng.maurodatamapper.dita.elements.langref.base.Topic
+import uk.ac.ox.softeng.maurodatamapper.dita.enums.Toc
+import uk.ac.ox.softeng.maurodatamapper.dita.html.HtmlHelper
+import uk.ac.ox.softeng.maurodatamapper.dita.processor.DitaProcessor
+
+import org.xmlunit.builder.DiffBuilder
+import org.xmlunit.builder.Input
+import org.xmlunit.diff.Diff
+import spock.lang.Specification
+
+class DitaProcessorSpecification extends Specification{
+
+    def setup() {
+    }
+
+    def cleanup() {
+
+    }
+
+
+    void "Test simple pdf generation"() {
+
+        when:
+        Topic testTopic = Topic.build(
+            id: "myFirstTopic"
+        ) {
+            title "My first topic"
+            body {
+                p {
+                    b "Hello, "
+                    txt "World!"
+                }
+            }
+        }
+
+        DitaProject ditaProject = new DitaProject(
+            filename: "myFirstDitaProject",
+            title: "My First DITA Project"
+        )
+
+        ditaProject.addTopic("", testTopic, Toc.YES)
+
+        byte[] fileContents = DitaProcessor.generatePdf(ditaProject)
+
+        then:
+
+        fileContents.size() == 7771 // The number of bytes of the generated pdf file
+
+    }
+
+
+}
