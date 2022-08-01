1f63501b4ce7b20f810c1550e54dd8a297644f3a
James Welch <james.welch@cs.ox.ac.uk>
2022-08-01 21:30:33 +0100

    Fix to html editor for div replacement

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/dita/html/HtmlHelper.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/dita/html/HtmlHelper.groovy
@@ -165,7 +165,7 @@ class HtmlHelper {
                 }
             case 'span':
                 return {
-                    div(updateAttributeMap(node), cl)
+                    ph(updateAttributeMap(node), cl)
                 }
             case 'em':
                 return {
